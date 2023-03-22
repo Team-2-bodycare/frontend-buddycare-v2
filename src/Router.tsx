5647026bc28cfd7d99ab4e275b36577713334cfa
GregorioGrGeOtOl <gregoriogrgeotavia@hotmail.com>
2023-03-21 23:39:53 -0300

    chore: add route replace

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { SignIn } from "./pages/SignIn/SignIn";
 import { SignUp } from "./pages/SignUp/SignUp";
 import Home from "./pages/home/Home";
@@ -7,6 +7,7 @@ export function Router() {
   return (
     <>
       <Routes>
+        <Route path="*" element={<Navigate to="/" replace />} />
         <Route path="/" element={<Home />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
