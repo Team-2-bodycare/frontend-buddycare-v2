66fe20f88503ced3b20918a96013dd0e96335a15
GregorioGrGeOtOl <gregoriogrgeotavia@hotmail.com>
2023-03-22 07:29:47 -0300

    chore: service create users

---
+++ src/services/users/UserSignUp.ts
@@ -0,0 +1,19 @@
+import { Api } from "../api/Api";
+import swal from "sweetalert";
+import { ISignUp } from "../../interfaces/ISignUp";
+
+export const RegisterService = {
+  Register: async (values: ISignUp) => {
+    try {
+      const res = await Api.post("/user", values);
+      return res;
+    } catch (error: any) {
+      swal({
+        title: "Error",
+        text: `${error.message}`,
+        icon: "error",
+        timer: 6000,
+      });
+    }
+  },
+};
