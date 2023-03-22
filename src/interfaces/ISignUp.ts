9432a6fab791ece1a2b520f02c3eeb84df303ca2
GregorioGrGeOtOl <gregoriogrgeotavia@hotmail.com>
2023-03-22 07:26:50 -0300

    feat: create interface signup

---
+++ src/interfaces/ISignUp.ts
@@ -0,0 +1,10 @@
+export interface ISignUp {
+  name: string;
+  email: string;
+  phone: string;
+  password: string;
+  photo?: string;
+  createdAt: Date;
+  updatedAt: Date;
+  isPsychologist: boolean;
+}
