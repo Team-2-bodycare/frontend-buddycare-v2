c2bf6a1bc85b50f0be35a0122c54ebd1900a35dd
GregorioGrGeOtOl <gregoriogrgeotavia@hotmail.com>
2023-03-24 11:28:44 -0300

    feat: create menu patient

---
+++ src/components/menuPatient/MenuPatient.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { IPatient } from "../../interfaces/IPatient";
+import { getByIdPatient } from "../../services/patient/ByIdPatient";
+import {
+  MenuContainer,
+  MenuDropDownContent,
+  MenuDropDownLi,
+  MenuSubA,
+  PatientPhoto,
+} from "./styleMenuPatient";
+
+export function MenuPatient() {
+  const navigate = useNavigate();
+  const userId = localStorage.getItem("userId");
+
+  const [patient, setPatient] = useState<IPatient>();
+  useEffect(() => {
+    patientById();
+  }, []);
+
+  async function patientById() {
+    const response = await getByIdPatient(`${userId}`);
+    setPatient(response.data);
+  }
+
+  const logout = () => {
+    localStorage.removeItem("jwt");
+    localStorage.removeItem("userId");
+    localStorage.removeItem("type");
+    navigate("/");
+  };
+
+  return (
+    <MenuContainer>
+      <MenuDropDownLi>
+        <PatientPhoto src={patient?.photo} />
+        <MenuDropDownContent>
+          <MenuSubA onClick={() => navigate("/signin")}>Editar Perfil</MenuSubA>
+          <MenuSubA onClick={() => navigate("/signin")}>Meu psicólogo</MenuSubA>
+          <MenuSubA onClick={() => navigate("/signin")}>Chat</MenuSubA>
+          <MenuSubA onClick={() => logout()}>Sair</MenuSubA>
+        </MenuDropDownContent>
+      </MenuDropDownLi>
+    </MenuContainer>
+  );
+}
