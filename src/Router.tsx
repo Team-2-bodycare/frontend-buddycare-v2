c71376f3027f460f717e8e65b22017fc02d4052c
GregorioGrGeOtOl <gregoriogrgeotavia@hotmail.com>
2023-03-24 11:19:40 -0300

    chore: protected routes

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,6 +4,9 @@ import { SignUp } from "./components/signUp/SignUp";
 import Home from "./pages/home/Home";
 import { PsychologistProfile } from "./pages/PsychologistProfile/PsychologistProfile";
 import { PatientProfile } from "./pages/patient/PatientProfile";
+import { ListPsychologist } from "./components/listPsychologist/listPsychologist";
+import { Detalhe } from "./pages/patient/details/Details";
+import { ProtectedRoutes } from "./components/protectedRoutes/ProtectedRoutes";
 
 export function Router() {
   return (
@@ -13,8 +16,12 @@ export function Router() {
       <Route path="/signin" element={<SignIn />} />
       <Route path="/signup-psychologist" element={<SignUp typeUser={true} />} />
       <Route path="/signup-patient" element={<SignUp typeUser={false} />} />
-      <Route path="/patient-profile" element={<PatientProfile />} />
+      <Route path="/" element={<ProtectedRoutes/>}>
+        <Route path="/patient-profile" element={<PatientProfile />} />
+      </Route>
       <Route path="/psychologist-profile" element={<PsychologistProfile />} />
+      <Route path="/list-psychologist" element={<ListPsychologist />} />
+      <Route path="/detalhe/:id" element={<Detalhe />} />
     </Routes>
   );
 }
