748510b00b76d3436704244aad46d14221ff8bea
GregorioGrGeOtOl <gregoriogrgeotavia@hotmail.com>
2023-03-26 21:52:53 -0300

    feat: create initial payment

---
+++ src/components/payment/Payment.tsx
@@ -0,0 +1,84 @@
+import { useState } from "react";
+import {
+  PaymentContainer,
+  PaymentInstallments,
+  PaymentInstallmentsDiv,
+  PaymentInstallmentsOptions,
+  PaymentLabel,
+  PaymentPriceInstallments,
+  PaymentPriceSession,
+  PaymentPriceTotal,
+  PaymentSessionDiv,
+  PaymentTotalSession,
+  PaymentTotalSessionOptions,
+} from "./stylePayment";
+
+interface IPayment {
+  totalSession: number;
+  priceSession: number;
+  installments: number;
+  priceInstallments: number;
+  priceTotal: number;
+}
+
+export function Payment() {
+  const [values, setValues] = useState({
+    priceSession: 100,
+    totalSession: 0,
+    priceTotal: 0,
+    installments: 0,
+    priceInstallments: 0,
+  });
+
+  const handleChangesValues = (
+    e: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    setValues((values: IPayment) => ({
+      ...values,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  return (
+    <PaymentContainer>
+      <PaymentPriceSession>Valor por sessão: R${(values.priceSession).toFixed(2).toString().replace(".", ",")}</PaymentPriceSession>
+      <PaymentLabel>Total de sessões </PaymentLabel>
+        <PaymentTotalSession name="totalSession"  onChange={handleChangesValues}>
+          <PaymentTotalSessionOptions></PaymentTotalSessionOptions>
+          <PaymentTotalSessionOptions value={5}>5</PaymentTotalSessionOptions>
+          <PaymentTotalSessionOptions value={6}>6</PaymentTotalSessionOptions>
+          <PaymentTotalSessionOptions value={7}>7</PaymentTotalSessionOptions>
+          <PaymentTotalSessionOptions value={8}>8</PaymentTotalSessionOptions>
+          <PaymentTotalSessionOptions value={9}>9</PaymentTotalSessionOptions>
+          <PaymentTotalSessionOptions value={10}>10</PaymentTotalSessionOptions>
+        </PaymentTotalSession>
+        
+      <PaymentInstallmentsDiv>
+        <PaymentLabel>Parcelado em</PaymentLabel>
+        <PaymentInstallments name="installments" onChange={handleChangesValues}>
+          <PaymentInstallmentsOptions></PaymentInstallmentsOptions>
+          <PaymentInstallmentsOptions value={1}>1</PaymentInstallmentsOptions>
+          <PaymentInstallmentsOptions value={2}>2</PaymentInstallmentsOptions>
+          <PaymentInstallmentsOptions value={3}>3</PaymentInstallmentsOptions>
+          <PaymentInstallmentsOptions value={4}>4</PaymentInstallmentsOptions>
+          <PaymentInstallmentsOptions value={5}>5</PaymentInstallmentsOptions>
+          <PaymentInstallmentsOptions value={6}>6</PaymentInstallmentsOptions>
+          <PaymentInstallmentsOptions value={7}>7</PaymentInstallmentsOptions>
+          <PaymentInstallmentsOptions value={8}>8</PaymentInstallmentsOptions>
+          <PaymentInstallmentsOptions value={9}>9</PaymentInstallmentsOptions>
+          <PaymentInstallmentsOptions value={10}>10</PaymentInstallmentsOptions>
+          <PaymentInstallmentsOptions value={11}>11</PaymentInstallmentsOptions>
+          <PaymentInstallmentsOptions value={12}>12</PaymentInstallmentsOptions>
+        </PaymentInstallments>
+        {
+          values.installments === 0 ? (
+            <p>x de R$0,00</p>
+          ) : (
+            <PaymentPriceInstallments>x de R${((values.priceSession * values.totalSession) / values.installments).toFixed(2).toString().replace(".", ",")}</PaymentPriceInstallments>
+          )
+        }
+      </PaymentInstallmentsDiv>
+      <PaymentPriceTotal>Total a pagar: R${(values.priceSession * values.totalSession).toFixed(2).toString().replace(".", ",")}</PaymentPriceTotal>
+    </PaymentContainer>
+  );
+}
---
+++ src/components/payment/stylePayment.ts
@@ -0,0 +1,57 @@
+import styled from "styled-components";
+
+export const PaymentContainer = styled.div`
+  border: 1px solid rgba(0, 0, 0, 0.3);
+  width: 100%;
+  border-radius: 10px;
+  padding: 10px;
+  font-weight: bold;
+  color: rgb(115, 115, 115);
+`;
+
+export const PaymentSessionDiv = styled.div`
+  display: flex;
+  gap: 10px;
+  margin-bottom: 10px;
+`;
+
+export const PaymentInstallmentsDiv = styled.div`
+  display: flex;
+  gap: 10px;
+  margin-bottom: 10px;
+`;
+
+export const PaymentPriceSession = styled.p`
+  margin-bottom: 10px;
+`;
+
+export const PaymentLabel = styled.label``;
+
+export const PaymentTotalSession = styled.select`
+  width: 40px;
+  height: 18px;
+  outline: none;
+  border: none;
+  border-radius: 5px;
+  color: rgb(115, 115, 115);
+  margin-bottom: 10px;
+`;
+
+export const PaymentTotalSessionOptions = styled.option``;
+
+export const PaymentInstallments = styled.select`
+  width: 40px;
+  height: 18px;
+  outline: none;
+  border: none;
+  border-radius: 5px;
+  color: rgb(115, 115, 115);
+`;
+
+export const PaymentInstallmentsOptions = styled.option``;
+
+export const PaymentPriceInstallments = styled.p``;
+
+export const PaymentPriceTotal = styled.p``;
+
+export const PaymentSumTotal = styled.p``;
