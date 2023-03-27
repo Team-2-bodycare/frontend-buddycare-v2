b1fca18f84b48619511b71e12147c39197179945
GregorioGrGeOtOl <gregoriogrgeotavia@hotmail.com>
2023-03-26 21:49:17 -0300

    chore: add new attributes

--- src/interfaces/IPatient.ts
+++ src/interfaces/IPatient.ts
@@ -5,4 +5,19 @@ export interface IPatient {
   phone: string;
   photo: string;
   userId: string;
+  psychologist: {
+    psychologist: {
+      id: string;
+      crp: string;
+      specialization?: string;
+      summary: string;
+      user: {
+        id: string;
+        name: string;
+        email: string;
+        phone: string;
+        photo: string;
+      };
+    };
+  };
 }
