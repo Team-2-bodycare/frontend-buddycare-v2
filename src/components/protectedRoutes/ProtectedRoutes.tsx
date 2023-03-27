4067a742371342faaa8464712ab603c4f277827d
GregorioGrGeOtOl <gregoriogrgeotavia@hotmail.com>
2023-03-27 08:51:06 -0300

    chore: add message error

--- src/components/protectedRoutes/ProtectedRoutes.tsx
+++ src/components/protectedRoutes/ProtectedRoutes.tsx
@@ -1,8 +1,18 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { isAuthenticated } from "../../services/isAuthenticated/IsAuthenticated";
+import swal from "sweetalert";
 
 export const ProtectedRoutes = (props: any) => {
   const logged = isAuthenticated();
+
+  if (!logged) {
+    swal({
+      title: "Erro de autenticação!",
+      text: "Entre com suas credenciais para acessar essa página!",
+      icon: "error",
+      timer: 5000,
+    });
+  }
 
   return logged ? <Outlet /> : <Navigate to="/" />;
 };
