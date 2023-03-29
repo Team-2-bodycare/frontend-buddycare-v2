da942e869131da4640c7cbbb6a2d3f16c7262a5d
GregorioGrGeOtOl <gregoriogrgeotavia@hotmail.com>
2023-03-29 12:59:30 -0300

    chore: add loading

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { Loading } from "../loading/Loading";
 import {
   MenuA,
   MenuContainer,
@@ -13,21 +15,39 @@ import {
 export function Menu() {
   const navigate = useNavigate();
 
+  let [loadind, setLoading] = useState(0);
+
+  const loadingShow = (props: string) => {
+    setLoading(1);
+    setTimeout(() => {
+      navigate(props);
+    }, 3000);
+  };
+
   return (
     <MenuContainer>
+      {loadind === 1 ? <Loading /> : <></>}
       <MenuUl>
         <MenuDropDownLi>
           <MenuDropbtn>Psicólogo</MenuDropbtn>
           <MenuDropDownContent>
-            <MenuSubA onClick={() => navigate("/signin")} >Entrar</MenuSubA>
-            <MenuSubA onClick={() => navigate("signup-psychologist")} >Registrar-se</MenuSubA>
+            <MenuSubA onClick={() => loadingShow("/signin")}>
+              Entrar
+            </MenuSubA>
+            <MenuSubA
+              onClick={() => loadingShow("/signup-psychologist")}
+            >
+              Registrar-se
+            </MenuSubA>
           </MenuDropDownContent>
         </MenuDropDownLi>
         <MenuDropDownLi>
           <MenuDropbtn>Paciente</MenuDropbtn>
           <MenuDropDownContent>
-            <MenuSubA onClick={() => navigate("/signin")} >Entrar</MenuSubA>
-            <MenuSubA onClick={() => navigate("/signup-patient")} >Registrar-se</MenuSubA>
+            <MenuSubA onClick={() => navigate("/signin")}>Entrar</MenuSubA>
+            <MenuSubA onClick={() => navigate("/signup-patient")}>
+              Registrar-se
+            </MenuSubA>
           </MenuDropDownContent>
         </MenuDropDownLi>
       </MenuUl>
