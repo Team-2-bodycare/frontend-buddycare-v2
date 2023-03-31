a32ecc4b2a0317ae8895b28cb78d0dd8e63251ea
Atila Mariano <103537702+atilamariano@users.noreply.github.com>
2023-03-31 02:59:34 -0300

    style: implement modal patient

--- src/components/modal/PatientModal.tsx
+++ src/components/modal/PatientModal.tsx
@@ -1,63 +1,69 @@
 import { IUser } from '../../interfaces/IPsychologistsProfile';
 import Modal from 'react-modal';
-import { Graphic } from '../graphic/Graphic';
+// import { Graphic } from '../graphic/Graphic';
 import { useState } from 'react';
 import { CommentNote } from '../comment/CommentNote';
+import { ModalGrid, StyleChat, } from './style/StylePatientModal';
 
 interface PatientModalProps {
-    patient: IUser | null;
-    onClose: () => void;
+  patient: IUser | null;
+  onClose: () => void;
 }
 
 export default function PatientModal({ patient, onClose }: PatientModalProps) {
-    const [progressData, setProgressData] = useState<{
-        labels: string[];
-        datasets: {
-          label: string;
-          data: number[];
-          fill: boolean;
-          borderColor: string;
-          tension: number;
-        }[];
-      }>({
-        labels: [],
-        datasets: [
-          {
-            label: 'Progresso',
-            data: [],
-            fill: false,
-            borderColor: 'rgba(75,192,192,1)',
-            tension: 0.1,
-          },
-        ],
-      });
+  const [progressData, setProgressData] = useState<{
+    labels: string[];
+    datasets: {
+      label: string;
+      data: number[];
+      fill: boolean;
+      borderColor: string;
+      tension: number;
+    }[];
+  }>({
+    labels: [],
+    datasets: [
+      {
+        label: 'Progresso',
+        data: [],
+        fill: false,
+        borderColor: 'rgba(75,192,192,1)',
+        tension: 0.1,
+      },
+    ],
+  });
 
 
 
-    if (!patient) {
-        return null;
-    }
+  if (!patient) {
+    return null;
+  }
 
-    return (
-        <Modal isOpen={true}>
-            <h2>Detalhes do Paciente</h2>
-            <div>
-                <img style={{ width: '150px', borderRadius: '49%' }} src={patient?.photo} alt="Avatar" />
-                <h3>{patient?.name}</h3>
-                <p>
-                    <b>Email:</b> {patient?.email}
-                </p>
-                <p>
-                    <b>Telefone:</b> {patient?.phone}
-                </p>
-                <p>
-                    <b>Progresso:</b> {patient?.worker?.patient[0].user.progress.join(', ')}
-                </p>
-            </div>
-            <button onClick={onClose}>Fechar</button>
+  return (
+    
+    <Modal isOpen={true}>
+      <ModalGrid>
+      <h2>Detalhes do Paciente</h2>
+      <div>
+        <img style={{ width: '150px', borderRadius: '49%' }} src={patient?.photo} alt="Avatar" />
+        <h3>{patient?.name}</h3>
+        <p>
+          <b>Email:</b> {patient?.email}
+        </p>
+        <p>
+          <b>Telefone:</b> {patient?.phone}
+        </p>
+        <p>
+          <b>Progresso:</b> {patient?.worker?.patient[0].user.progress.join(', ')}
+        </p>
+      </div>
+      <button onClick={onClose}>Fechar</button>
 
-            <CommentNote/>
-            {/* <Graphic labels={data.labels} datasets={data.datasets} /> */}
-        </Modal>
-    );
+      {/* <Graphic data={data} options={Option} /> */}
+      <StyleChat>
+        <CommentNote />
+      </StyleChat>
+      </ModalGrid>
+    </Modal>
+  );
 }
---
+++ src/components/modal/style/StylePatientModal.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const ModalGrid = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: auto;
+  height: 28rem;
+  
+`;
+
+
+export const StyleChat = styled.div`
+width: auto;
+height: 25rem;
+justify-content: center;
+display: flex;
+background-color: #0077b3;
+border-radius: 10px;
+`;
